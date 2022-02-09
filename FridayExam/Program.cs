95148d5b3adb552c140ee5edde49d864b82c14ce
NagababuVeganti <77457090+NagababuVeganti@users.noreply.github.com>
2022-02-08 20:07:59 -0500

    Updated Exam

--- FridayExam/Program.cs
+++ FridayExam/Program.cs
@@ -9,105 +9,58 @@ namespace FridayExam
         {
             //Question 1
 
+            int N = 6;
+            PrintPattern(N);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Question 2
+
             int[] nums = new int[] { 1, 1, 1 };
             int operations = MinOperations(nums);
             Console.WriteLine("Minium operations Required are {0}", operations);
             Console.WriteLine();
             Console.WriteLine();
 
-            //Question 2
-
-            string bulls_string = "abbaca";
-            Console.WriteLine("Final After removing duplicates is {0}", RemoveDuplicates(bulls_string));
-
-            Console.WriteLine();
-            Console.WriteLine();
-
             //Question 3
 
-            string[] words = { "bella", "label", "roller" };
-            List<char> common_words = CommonChars(words);
-            Console.WriteLine("Common characters are:");
-            foreach (char c in common_words)
+            string[] words = {"eat","tea","tan","ate","nat","bat"};
+            List<List<string>> groups = WordGroups(words);
+            Console.WriteLine("Groups are ");
+            foreach (List<string> G in groups)
             {
-                Console.Write(c + " ");
+                foreach(string s in G )
+                {
+                    Console.Write(s + "  ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+
             }
 
             Console.WriteLine();
             Console.WriteLine();
 
-
-            //Question 4
-
-            string text = "mumacollegeofbusiness";
-            Console.WriteLine("Number of USF strings that can be constructed are {0}", CountUsf(text));
-            Console.WriteLine();
-            Console.WriteLine();
-
-            //Question 5
-
-            string s1 = "anagram";
-            string t = "nagaram";
-            bool flag1 = IsAnagram(s1, t);
-            if(flag1)
-            Console.WriteLine("Given two strings are anagrams");
-            else
-            Console.WriteLine("Given two strings are not anagrams");
-
-            Console.WriteLine();
-            Console.WriteLine();
 
         }
 
         //Question 1
 
-        public static int MinOperations(int[] arr)
+        public static void PrintPattern(int N)
         {
             try
             {
 
-                return 0;
             }
             catch (Exception e)
             {
-                throw e;
+                throw;
             }
         }
 
         //Question 2
 
-        public static string RemoveDuplicates(string s)
-        {
-            try
-            {
-                return "";
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-        }
-
-        //Question 3
-
-        public static List<char> CommonChars(string[] words)
-        {
-            List<char> common_words = new List<char>();
-
-            try
-            {
-                return common_words;
-
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-        }
-
-        //Question 4
-
-        public static int CountUsf(string s)
+        public static int MinOperations(int[] nums)
         {
             try
             {
@@ -115,21 +68,24 @@ namespace FridayExam
             }
             catch (Exception e)
             {
-                throw e;
+                throw;
             }
         }
 
-        //Question 5
+        //Question 3
 
-        public static bool IsAnagram(string s, string t)
+        public static List<List<string>> WordGroups(string[] words)
         {
+            List<List<string>> groups = new List<List<string>>();
+
             try
             {
-                return false;
+                return groups;
+
             }
             catch (Exception e)
             {
-                throw e;
+                throw;
             }
         }
     }
